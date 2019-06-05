065994a75ffc8da40c7f23568cefdee0c8a1e475
Kieran <hello_kieran@foxmail.com>
2019-06-05 15:30:51 +0800

    vetur bug

---
+++ render-bug.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <template
+      >TEST</template
+    >
+  </div>
+</template>
+
+<style scoped>
+div {
+  color: red;
+  font-size: 12px;
+  display: inline-block;
+}
+</style>
+
+<script>
+import Vue from 'vue';
+</script>
